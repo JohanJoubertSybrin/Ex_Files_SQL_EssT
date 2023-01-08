89dbe81db0c67f8ac698d4db1a23a0db9cb45d41
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-08 17:47:03 +0200

    Create Inserting data.sql

---
+++ Exercise Files/Example SQL/Inserting data.sql
@@ -0,0 +1,7 @@
+SELECT * FROM customer;
+
+INSERT INTO customer (name, address, city, state, zip) 
+  VALUES ('Fred Flintstone', '123 Cobblestone Way', 'Bedrock', 'CA', '91234');
+
+INSERT INTO customer (name, city, state) 
+  VALUES ('Jimi Hendrix', 'Renton', 'WA');
