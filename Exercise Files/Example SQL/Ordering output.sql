ca1c216afaf979dd4f7539276f845cd4443d5ee1
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-08 21:15:11 +0200

    Create Ordering output.sql

---
+++ Exercise Files/Example SQL/Ordering output.sql
@@ -0,0 +1,11 @@
+SELECT Name FROM Country;
+
+SELECT Name FROM Country ORDER BY Name;
+
+SELECT Name FROM Country ORDER BY Name DESC;
+
+SELECT Name FROM Country ORDER BY Name ASC;
+
+SELECT Name, Continent FROM Country ORDER BY Continent, Name;
+
+SELECT Name, Continent, Region FROM Country ORDER BY Continent DESC, Region, Name;
