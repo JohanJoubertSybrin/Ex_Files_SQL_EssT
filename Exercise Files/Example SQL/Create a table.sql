578ddc67bd2711ed4a3549522b03228bacb82b3f
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-08 20:23:36 +0200

    create a table

---
+++ Exercise Files/Example SQL/Create a table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE test(
+    a INTEGER,
+    b TEXT
+);
+
+INSERT INTO test VALUES ( 1, 'a');
+INSERT INTO test VALUES ( 2, 'b');
+INSERT INTO test VALUES ( 3, 'c');
+
+SELECT * FROM test;
