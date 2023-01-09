7f86e50460828b44ed3a632989bb798ecb9b9e2f
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-09 07:25:00 +0200

    Create Folding case.sql

---
+++ Exercise Files/Example SQL/Folding case.sql
@@ -0,0 +1,9 @@
+SELECT 'StRiNg' = 'string';
+
+SELECT LOWER('StRiNg') = LOWER('string');
+
+SELECT UPPER('StRiNg') = UPPER('string');
+
+SELECT UPPER(Name) FROM City ORDER BY Name;
+
+SELECT LOWER(Name) FROM City ORDER BY Name;
