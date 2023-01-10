6d1681928215de2a3170e113cd1a4492b48e8689
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-10 08:24:55 +0200

    Create Automating data with triggers.sql

---
+++ Exercise Files/Example SQL/Automating data with triggers.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS widgetCustomer;
+DROP TABLE IF EXISTS widgetSale;
+
+CREATE TABLE widgetCustomer ( id INTEGER PRIMARY KEY, name TEXT, last_order_id INT );
+CREATE TABLE widgetSale ( id INTEGER PRIMARY KEY, item_id INT, customer_id INT, quan INT, price INT );
+
+INSERT INTO widgetCustomer (name) VALUES ('Bob');
+INSERT INTO widgetCustomer (name) VALUES ('Sally');
+INSERT INTO widgetCustomer (name) VALUES ('Fred');
+
+SELECT * FROM widgetCustomer;
+
+CREATE TRIGGER newWidgetSale AFTER INSERT ON widgetSale
+    BEGIN
+        UPDATE widgetCustomer SET last_order_id = NEW.id WHERE widgetCustomer.id = NEW.customer_id;
+    END
+;
+
+INSERT INTO widgetSale (item_id, customer_id, quan, price) VALUES (1, 3, 5, 1995);
+INSERT INTO widgetSale (item_id, customer_id, quan, price) VALUES (2, 2, 3, 1495);
+INSERT INTO widgetSale (item_id, customer_id, quan, price) VALUES (3, 1, 1, 2995);
+SELECT * FROM widgetSale;
+SELECT * FROM widgetCustomer;
